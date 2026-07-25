5f8cf6b05b303e2061d5293a4fbd4cf7c98e286f
sytom <nguyentiensy.nts.9796@gmail.com>
2026-07-25 21:05:49 +0700

    style: test Tự động kiểm tra API sau Deploy

--- src/Product.Api/Controllers/ProductsController.cs
+++ src/Product.Api/Controllers/ProductsController.cs
@@ -62,6 +62,7 @@ public class ProductsController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetProductList(CancellationToken cancellationToken)
     {
+        return StatusCode(500);
         var productList = await _context.Products
             .Select(p => new ProductResponse
             {
