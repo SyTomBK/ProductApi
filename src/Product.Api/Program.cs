51a372725866177c99840ce555114f0542f8df85
sytom <nguyentiensy.nts.9796@gmail.com>
2026-08-24 08:24:21 +0700

    feat: thêm phần migration khi build

--- src/Product.Api/Program.cs
+++ src/Product.Api/Program.cs
@@ -26,6 +26,13 @@ builder.Services
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    db.Database.Migrate();
+}
+
 app.UseHttpMetrics();
 
 app.UseSerilogRequestLogging();
